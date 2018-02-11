6ea56ae768be7ba5a34a7dc206d6da28d1ce8d44
chrismathew3 <chrismathew3@gmail.com>
2018-02-11 08:36:16 -0500

    Updated main

--- Assignment-1-Song-Library/src/application/Main.java
+++ Assignment-1-Song-Library/src/application/Main.java
@@ -30,6 +30,9 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show(); 
 			
+			// Get the controller and pass off control
+			ViewController vc = loader.getController();
+			
 			
 			
 		} catch(IOException e) {
