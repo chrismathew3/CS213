8dcffbf4bd0d29c22a32b2d4ddf51a11c6471a82
chrismathew3 <chrismathew3@gmail.com>
2018-02-11 23:25:43 -0500

    Created Song Class

---
+++ Assignment-1-SongLib/src/application/Song.java
@@ -0,0 +1,58 @@
+package application;
+
+public class Song {
+
+	String name;
+	String artist;
+	String album;
+	String year;
+	
+	public Song(String name, String artist) {
+		this.name = name;
+		this.artist = artist;
+	}
+	
+	public Song(String name, String artist, String album, String year) {
+		this.name = name;
+		this.artist = artist;
+		this.album = album;
+		this.year = year;
+	}
+	
+	public boolean editName(String name) {
+		if(name != null) {
+			this.name = name;
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public boolean editAlbum(String album) {
+		if(album != null) {
+			this.album = album;
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public boolean editArtist(String artist) {
+		if(artist != null) {
+			this.artist = artist;
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public boolean editYear(String year) {
+		if(year != null) {
+			this.year = year;
+			return true;
+		}else {
+			return false;
+		}
+	}
+
+
+	
+	
+}
--- Assignment-1-SongLib/src/view/SongLibController.java
+++ Assignment-1-SongLib/src/view/SongLibController.java
@@ -1,7 +1,10 @@
 package view;
 
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -9,7 +12,8 @@ import javafx.scene.control.TextField;
 
 
 
-public class SongLibController {
+public class SongLibController  {
+	
 	
 	@FXML ListView<String> listViewName;
 	@FXML ListView<String> listViewArtist;
@@ -24,10 +28,37 @@ public class SongLibController {
 	private ObservableList<String> obsListName;
 	private ObservableList<String> obsListArtist;
 	public void start() {
-		obsListName = FXCollections.observableArrayList("Chris", "Mathew");
+		obsListName = FXCollections.observableArrayList("Stronger");
 		listViewName.setItems(obsListName);
-		obsListArtist = FXCollections.observableArrayList("Mathew", "Chris");
+		obsListArtist = FXCollections.observableArrayList("Kanye West");
 		listViewArtist.setItems(obsListArtist);
+		newSong.setOnAction(newbuttonHandler);
+		deleteSong.setOnAction(deletebuttonHandler);
+		editSong.setOnAction(editbuttonHandler);
 	}
+	
+	EventHandler<ActionEvent> newbuttonHandler = new EventHandler<ActionEvent>() {
+	    @Override
+	    public void handle(ActionEvent event) {
+	    		System.out.println("HERE");
+	    		event.consume();
+	    }
+	};
+	EventHandler<ActionEvent> editbuttonHandler = new EventHandler<ActionEvent>() {
+	    @Override
+	    public void handle(ActionEvent event) {
+	        //label.setText("Accepted");
+	    		System.out.println("HERE");
+	        event.consume();
+	    }
+	};
+	EventHandler<ActionEvent> deletebuttonHandler = new EventHandler<ActionEvent>() {
+	    @Override
+	    public void handle(ActionEvent event) {
+	        //label.setText("Accepted");
+	    		System.out.println("HERE");
+	        event.consume();
+	    }
+	};
 	
 }
