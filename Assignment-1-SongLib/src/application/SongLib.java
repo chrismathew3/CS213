d98bfb80f19626136667d3cdfa1e5b17bb9ce1ca
chrismathew3 <chrismathew3@gmail.com>
2018-02-14 00:46:30 -0500

    First take

--- Assignment-1-SongLib/src/application/SongLib.java
+++ Assignment-1-SongLib/src/application/SongLib.java
@@ -2,29 +2,27 @@ package application;
 	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-import view.SongLibController;;
+//import view.SongLibController;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+//import javafx.scene.layout.AnchorPane;
 
 
-
-public class Main extends Application {
-	
-	
+public class SongLib extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(getClass().getResource("/view/SongLib.fxml"));
-			AnchorPane root = (AnchorPane)loader.load();
-			SongLibController slc = loader.getController();
-			slc.start();
-			Scene s = new Scene(root, 650, 300);
-			primaryStage.setScene(s);
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
+			//SongLibController slc = loader.getController();
+			//slc.initialize();
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Song Library");
+			primaryStage.setResizable(false);
 			primaryStage.show();
-			
-			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
--- Assignment-1-SongLib/src/view/SongLibController.java
+++ Assignment-1-SongLib/src/view/SongLibController.java
@@ -1,173 +1,117 @@
 package view;
 
+import java.util.Optional;
 
-
-
-import java.util.ArrayList;
-import java.util.List;
-
-import application.Main;
 import application.Song;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
-
-
-public class SongLibController  {
+public class SongLibController {
+	@FXML AnchorPane Base;
+	@FXML TableView<Song> SongTable;
+	@FXML TableColumn<Song,String> SongColumn;
+	@FXML TableColumn<Song,String> ArtistColumn;
+	@FXML TextField SongField;
+	@FXML TextField ArtistField;
+	@FXML TextField AlbumField;
+	@FXML TextField YearField;
+	@FXML Text SongDetailsText; 
+	@FXML Text SongText;
+	@FXML Text ArtistText;
+	@FXML Text AlbumText;
+	@FXML Text YearText;
+	@FXML Button AddButton;
+	@FXML Button EditButton;
+	@FXML Button DeleteButton;
 	
+	ObservableList<Song> tableList = FXCollections.observableArrayList();
 	
-	@FXML TableView<Song> songTable;
-	@FXML TableColumn<Song, String> nameColumn;
-	//@FXML TableColumn<Song, String> tableColArtist;
-	@FXML Button newSong;
-	@FXML Button deleteSong;
-	@FXML Button editSong;
-	@FXML TextField enterSong;
-	@FXML TextField enterArtist;
-	@FXML TextField enterAlbum;
-	@FXML TextField enterYear;
-	public ObservableList<Song> data;
-	//private ObservableList<Song> obsListArtist;
-	public void start() {
-		songTable = new TableView<Song>();
-		nameColumn = new TableColumn<Song,String>("Name");
-		nameColumn.setCellValueFactory(cellData -> cellData.getValue().getNameProperty());
-		data = getSongList();
-		songTable.setItems(data);
-		
-		// Select the first row
-		songTable.getSelectionModel().select(0);
-		songTable.getSelectionModel().getSelectedItem();
-		songTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showSongDetails(newValue));
-		
-
-		//tableColArtist = new TableColumn<Song, String>("Artist");
-		//List<Song> list = new ArrayList<Song>();
-		//list.add(new Song("Stronger", "Kanye West"));
-		//list.add(new Song("Real Friends", "Camilia Cabello"));
-		//data.add(list);
-		//tableColSong.setCellValueFactory(cellData -> cellData.getValue().getNameProperty());
-		//tableColArtist.setCellValueFactory(cellData -> cellData.getValue().getArtistProperty());
-		//tableView.setItems(data);
-		//tableColSong = new TableColumn<List<Song>, String>("Name");
-		//tableColSong.setCellValueFactory(new PropertyValueFactory("name"));
-		//tableColArtist.setCellValueFactory
-		//tableView.getColumns().setAll(tableColSong);
-		//tableView.setItems(obsListName);
-		//obsListArtist = FXCollections.observableArrayList(first);
-		//tableView.setItems();
-		//tableView.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showSongDetails(newValue));
-		newSong.setOnAction(newbuttonHandler);
-		deleteSong.setOnAction(deletebuttonHandler);
-		editSong.setOnAction(editbuttonHandler);
+	@FXML
+    public void initialize() {
+        SongColumn.setCellValueFactory(new PropertyValueFactory<Song, String>("song"));
+        ArtistColumn.setCellValueFactory(new PropertyValueFactory<Song, String>("artist"));
+        
+        // Listen for selection changes and show the song details when changed.
+        SongTable.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> showSongDetails(newValue));
+    }
+	
+	public ObservableList<Song> getSongData() {
+		return tableList;
 	}
 	
-	 private void showSongDetails(Song song) {
-		 // If song is null, that means no song is selected.
-		 if (song != null) {
-			 // Fill the text fields with info from the selected song object.
-			 enterSong.setText(song.getName(song));
-	         enterArtist.setText(song.getArtist(song));
-	         enterAlbum.setText(song.getAlbum(song));
-	         enterYear.setText(song.getYear(song));
-	     } else {
-	         // No song is selected, remove all the text.
-	         enterSong.setText("");
-	         enterArtist.setText("");
-	         enterAlbum.setText("");
-	         enterYear.setText("");
-	     }
-	 }
-	 private ObservableList<Song> getSongList() {
-		 
-		 Song one = new Song("Pray for me", "The Weekend", "Pray for Me","2018");
-		 Song two = new Song("Stronger", "Kanye West", "Graduation","2007");
-		 Song three = new Song("All the stars", "Kendrick Lamar", "All the Stars","2018");
-	 
-	     ObservableList<Song> list = FXCollections.observableArrayList(one, two, three);
-	     return list;
-	 }
+	private void showSongDetails(Song song) {
+    	// If song is null, that means no song is selected.
+        if (song != null) {
+            // Fill the text fields with info from the selected song object.
+            SongField.setText(song.getSong());
+            ArtistField.setText(song.getArtist());
+            AlbumField.setText(song.getAlbum());
+            YearField.setText(song.getYear());
+        } else {
+            // No song is selected, remove all the text.
+            SongField.setText("");
+            ArtistField.setText("");
+            AlbumField.setText("");
+            YearField.setText("");
+        }
+    }
+	@FXML
+	private void handleAddButton(ActionEvent event) {
+		if (SongField.getText().trim().isEmpty() || ArtistField.getText().trim().isEmpty()) {
+			Alert error = new Alert(Alert.AlertType.ERROR, "Please enter a valid Song AND Artist to add to the library", ButtonType.CLOSE);
+			error.showAndWait();
+			return;	
+		} else {
+			tableList.add(new Song(SongField.getText().trim(), ArtistField.getText().trim(), AlbumField.getText().trim(), YearField.getText().trim()));
+			SongTable.setItems(getSongData());
 	
-	EventHandler<ActionEvent> newbuttonHandler = new EventHandler<ActionEvent>() {
-	    @Override
-	    public void handle(ActionEvent event) {
-	    		@SuppressWarnings("unused")
-				Boolean isName;
-	    		@SuppressWarnings("unused")
-				Boolean isArtist;
-	    		//Boolean isAlbum;
-	    		//Boolean isYear;
-	    	
-	    		String name = enterSong.getText();
-			if(name.length() <= 1) {
-				Alert alert = new Alert(AlertType.INFORMATION);
-				alert.setTitle("Warning!");
-				alert.setHeaderText(null);
-				alert.setContentText("Enter the song name with at least 1 character!");
-				alert.showAndWait();
-				isName = false;
-			}else {
-				isName = true;
-			}
-			
-			String artist = enterArtist.getText();
-			if(artist.length() <= 1) {
-				Alert alert = new Alert(AlertType.INFORMATION);
-				alert.setTitle("Warning!");
-				alert.setHeaderText(null);
-				alert.setContentText("Enter artist with at least 1 character!");
-				alert.showAndWait();
-				isArtist = false;
-			}else {
-				isArtist = true;
-			}
-		/*
-			if((isName && isArtist) == true) {
-				Song temp = new Song(name, artist);
-				
-			
-				tempList = FXCollections.observableArrayList(temp);
-				ObservableList<Song> concatName = FXCollections.concat(tempList, obsListName);
-				obsListName = concatName;
-				listViewName.setItems(concatName);
-	    			System.out.println("NAME IS: " + temp.name + " Artist is: " + temp.artist + " ");
-			}else {
-				System.out.println("Alert Should of popped up!");
-			}
-		*/
-	    		event.consume();
-	    }
-	};
-	EventHandler<ActionEvent> editbuttonHandler = new EventHandler<ActionEvent>() {
-	    @Override
-	    public void handle(ActionEvent event) {
-	        //label.setText("Accepted");
-	    		System.out.println("HERE");
-	        event.consume();
-	    }
-	};
-	EventHandler<ActionEvent> deletebuttonHandler = new EventHandler<ActionEvent>() {
-	    @Override
-	    public void handle(ActionEvent event) {
-	        //label.setText("Accepted");
-	    		System.out.println("HERE");
-	        event.consume();
-	    }
-	};
+		}  
+	 }
+	@FXML
+	private void handleEditButton(ActionEvent event) {
+		
+	}
+	@FXML
+	private void handleDeleteButton(ActionEvent event) {
+		int index = SongTable.getSelectionModel().getSelectedIndex();
+		if(index < 0 || index > SongTable.getItems().size()) {
+			Alert error = new Alert(Alert.AlertType.ERROR, "No song selected.  Please select a valid song.", ButtonType.CLOSE);
+			error.showAndWait();
+			return;
+		}
+		//Confirmation Pop-Up
+		Alert confirm = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to delete the selected song?", ButtonType.YES, ButtonType.NO);
+		Optional<ButtonType> result = confirm.showAndWait();
+		if(result.get() == ButtonType.YES) {
+			SongTable.getItems().remove(index);
+			// If the deleted song isn't the last song in the list, then select the next song.
+			if (index < SongTable.getItems().size()) {
+        			SongTable.getSelectionModel().select(index);
+        			SongTable.getFocusModel().focus(index);
+        			showSongDetails(SongTable.getItems().get(index));
+            }else {
+            		// If the deleted song was the only song in the list, select nothing.
+            		if (SongTable.getItems().size() == 0) {
+            			showSongDetails(null);
+            		}else {
+            			// If the deleted song was the last song in the list, select the previous song.
+            			SongTable.getSelectionModel().select(index - 1);
+                		SongTable.getFocusModel().focus(index - 1);
+                		showSongDetails(SongTable.getItems().get(index - 1));
+            		}
+            }
+		}
 	
+	}
 }
